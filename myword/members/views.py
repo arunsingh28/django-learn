82111784adbdc5b596b1d7acf99cdc2a9c945a09
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-06-17 14:18:15 +0530

    model migration

--- myword/members/views.py
+++ myword/members/views.py
@@ -1,8 +1,12 @@
+from json import load
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello worlds')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
